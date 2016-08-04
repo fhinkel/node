18786d5036465c9ecf06f0b11e1e1e61fed55213
Franziska Hinkelmann <franzih@chromium.org>
2016-08-04 11:26:17 +0200

    test: add test for freeze() etc inside vm

--- test/parallel/test-vm-preserves-property.js
+++ test/parallel/test-vm-preserves-property.js
@@ -1,9 +1,8 @@
 'use strict';
 
 require('../common');
-var assert = require('assert');
-
-var vm = require('vm');
+const assert = require('assert');
+const vm = require('vm');
 
 var x = {};
 Object.defineProperty(x, 'prop', {
@@ -12,14 +11,44 @@ Object.defineProperty(x, 'prop', {
   writable: false,
   value: 'val'
 });
+
 var o = vm.createContext(x);
 
 var code = 'Object.getOwnPropertyDescriptor(this, "prop")';
 var res = vm.runInContext(code, o, 'test');
 
 assert(res);
-assert.equal(typeof res, 'object');
-assert.equal(res.value, 'val');
-assert.equal(res.configurable, false, 'should not be configurable');
-assert.equal(res.enumerable, false, 'should not be enumerable');
-assert.equal(res.writable, false, 'should not be writable');
+assert.strictEqual(typeof res, 'object');
+assert.strictEqual(res.value, 'val');
+assert.strictEqual(res.configurable, false, 'should not be configurable');
+assert.strictEqual(res.enumerable, false, 'should not be enumerable');
+assert.strictEqual(res.writable, false, 'should not be writable');
+
+
+code =
+  'Object.freeze(x);' +
+  'Object.isFrozen(x)';
+
+var sandbox = {x: {}};
+var context = vm.createContext(sandbox);
+assert.strictEqual(true, vm.runInContext(code, context));
+assert.strictEqual(true, Object.isFrozen(sandbox.x));
+
+
+code =
+  'Object.preventExtensions(y);' +
+  'Object.isExtensible(y)';
+
+sandbox = {y: {}};
+context = vm.createContext(sandbox);
+assert.strictEqual(false, vm.runInContext(code, context));
+assert.strictEqual(false, Object.isExtensible(sandbox.y));
+
+code =
+  'Object.seal(z);' +
+  'Object.isSealed(z)';
+
+sandbox = {z: {}};
+context = vm.createContext(sandbox);
+assert.strictEqual(true, vm.runInContext(code, context));
+assert.strictEqual(true, Object.isSealed(sandbox.z));
